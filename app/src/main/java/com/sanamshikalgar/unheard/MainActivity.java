e2cfdeac3dc044aecaadba01a0f7afa7acd518e8
Nasa <sana.m32291@gmail.com>
2018-07-19 15:36:47 -0400

    changes to MainActivity.java icon as per screen dimensions added

--- app/src/main/java/com/sanamshikalgar/unheard/MainActivity.java
+++ app/src/main/java/com/sanamshikalgar/unheard/MainActivity.java
@@ -1,12 +1,10 @@
 package com.sanamshikalgar.unheard;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,9 +21,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acti);
         // Assign the Views from the layout file to the corresponding variables
-        factTextView = (TextView) findViewById(R.id.factTextView);
-        showFactButton = (Button) findViewById(R.id.showFactButton);
-        linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactButton);
+        linearLayout = findViewById(R.id.linearLayout);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
@@ -41,8 +39,5 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         showFactButton.setOnClickListener(listener);
-
-        //Toast.makeText(this, "Yay! Our Activity was created!", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "We're logging from the onCreate() method!");
     }
 }
