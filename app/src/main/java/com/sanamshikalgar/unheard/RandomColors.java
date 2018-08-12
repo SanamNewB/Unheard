98c9fef8765e0db78abd21bc1bce03415fb8ae54
HereToLearn <40501545+SanamNewB@users.noreply.github.com>
2018-08-12 15:35:44 -0400

    Update RandomColors.java
    Lessened the colorful background to shades of Blue only

--- app/src/main/java/com/sanamshikalgar/unheard/RandomColors.java
+++ app/src/main/java/com/sanamshikalgar/unheard/RandomColors.java
@@ -7,19 +7,16 @@ import java.util.Random;
 public class RandomColors {
     // Fields or Member Variables - Properties about the object
     private String[] colors = {
-            "#39add1", // light blue
-            "#3079ab", // dark blue
-            "#c25975", // mauve
-            "#e15258", // red
-            "#f9845b", // orange
-            "#838cc7", // lavender
-            "#7d669e", // purple
-            "#53bbb4", // aqua
-            "#51b46d", // green
-            "#e0ab18", // mustard
-            "#637a91", // dark gray
-            "#f092b0", // pink
-            "#b7c0c7"  // light gray
+            "#7986cb",
+            "#c5cae9",
+            "#b39ddb",
+            "#9fa8da",
+            "#90caf9",
+            "#bbdefb",
+            "#42a5f5",
+            "#3f51b5",
+            "#303f9f",
+            "#1976d2"
     };
 
     // Methods - Actions the object can take
