bf1495302ceff994830339515b0bd224a8afdca0
HereToLearn <40501545+SanamNewB@users.noreply.github.com>
2018-08-12 15:23:24 -0400

    Update UnheardContent.java
    Adding logic to display previous random fact

--- app/src/main/java/com/sanamshikalgar/unheard/UnheardContent.java
+++ app/src/main/java/com/sanamshikalgar/unheard/UnheardContent.java
@@ -32,13 +32,43 @@ public class UnheardContent {
             "The American flag was designed by a high school student Bob Heft’ in 1958 as a school project",
             "The clothing retail shop was originally called Hennes—Swedish for “hers”—before acquiring the hunting and fishing equipment brand Mauritz Widforss. " +
                     "Eventually, Hennes & Mauritz was shortened to H&M.",
+            "Studies show spending hours reading notes to prepare for a test is ineffective. Flash cards, however, are excellent memory reinforecement.",
+            "STEM majors earn roughly $700,000 more over 40 years than social science or humanities majors.",
+            "Taller people are slightly more likely to get cancer than shorter people, due to the fact they have more cells to develop mutations.",
+            "First generation immigrants are less likely to commit crimes than natives and second generation children.",
+            "There is no \"24 hour waiting period\" on a missing persons report. If someone is missing, even if its for an hour, you can most ceratinly file a report.",
+            "An app called Woman Interrupted uses your smartphone's microphone to track how many times men interrupt women in a given conversation.",
+            "White House officials can't stop Trump from his habit of ripping up papers, even when they need to be preserved. So, they enlist employees to find pieces of documents in Trump's office and tape them back together.",
+            "Walking through a doorway can cause memory lapses, which is why we walk into another room, only to forget why we did.",
+            "Children develop the ability to see through lies when they are about seven years old.",
+            "The US may have adopted the metric system if pirates hadn't kidnapped the French scientist sent to help Thomas Jefferson persuade Congress to adopt the system.",
     };
 
     // Methods - Actions the object can take
     public String getFact() {
+        if (s != null){
+            stack.push(s);
+            s = null;
+        }
         // Randomly select a fact
         Random randomGenerator = new Random();
-        int randomNumber = randomGenerator.nextInt(facts.length);
-        return facts[randomNumber];
+        int i = randomGenerator.nextInt(facts.length);
+        String one = facts[i];
+        stack.push(facts[i]);
+        n = stack.size();
+        return one;
+    }
+    
+    public String getPreviousFact() {
+        if (!stack.empty() && (n == stack.size())) {
+            stack.pop();
+        }
+        if (!stack.empty()) {
+            s = stack.pop();
+            return s;
+        }
+        else {
+            return null;
+        }
     }
 }
